63f4ed51b2cc4d3df0f46ce8b5a88a95b1fa9856
Mingjing Yu <mingjingyu@microsoft.com>
2022-01-20 15:56:26 +0800

    thread

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleApp3
 {
@@ -7,6 +8,24 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting in the Main Method");
+
+            var t1 = new Thread(new ThreadStart(DoWork));
+            t1.Start();
+
+            var t2 = new Thread(new ThreadStart(DoWork));
+            t2.Start();
+
+            var t3 = new Thread(new ThreadStart(DoWork));
+            t3.Start();
+
+            Console.WriteLine("Ending of the Main Method");
+        }
+
+        private static void DoWork()
+        {
+            int x = 10;
+            Console.WriteLine("Managed Thread ID : {0} says {1}", Thread.CurrentThread.ManagedThreadId, x);
         }
     }
 }
